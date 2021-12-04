e9fb21c8643d561e6dd77f1025d329db4e0a760e
Nahuel Paladino <43184105+npala043@users.noreply.github.com>
2021-12-04 16:38:36 -0700

    almost done, needs anthony to finish site-side content for redirects

--- api/handlers/router.js
+++ api/handlers/router.js
@@ -1,6 +1,8 @@
+const helper = require('./helpers.js');
+
 // Returns all plays (DO NOT INCLUDE PLAYTEXT)
 const handleAllPlays = (app, Play) => {
-    app.route('/api/list').get((req, resp) => {
+    app.get('/api/list', (req, resp) => {
         Play.find({}, '-playText', (err, data) => {
             if (err) {
                 resp.json({ message: 'Unable to connect to plays' });
@@ -13,8 +15,7 @@ const handleAllPlays = (app, Play) => {
 
 // Returns single play (INCLUDE PLAYTEXT)
 const handleSinglePlay = (app, Play) => {
-    app.route('/api/list/:id')
-        .get((req, resp) => {
+    app.get('/api/list/:id', helper.ensureAuthenticated, (req, resp) => {
             Play.find({ id: req.params.id }, (err, data) => { // must be exact match
                 if (err) {
                     resp.json({ message: 'Play not found' });
@@ -27,8 +28,7 @@ const handleSinglePlay = (app, Play) => {
 
 // Returns single user (id, details, picture, membership, email)
 const handleSingleUser = (app, User) => {
-    app.route('/api/user/:id')
-        .get((req, resp) => {
+    app.get('/api/user/:id', helper.ensureAuthenticated, (req, resp) => {
             User.find({ id: req.params.id }, 'id details picture membership email', (err, data) => {
                 if (err) {
                     resp.json({ message: 'User not found'});
