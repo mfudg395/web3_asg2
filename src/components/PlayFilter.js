2fb4c20ac64ed55d65a37d3ee2b723c74318629f
Nahuel Paladino <43184105+npala043@users.noreply.github.com>
2021-12-09 15:44:34 -0700

    started antd form redesign, css needs fixing

--- src/components/PlayFilter.js
+++ src/components/PlayFilter.js
@@ -1,8 +1,10 @@
-import { Button, Input, Space } from 'antd';
-import {Select} from 'antd';
-import FormItem from 'antd/lib/form/FormItem';
-import React, {useEffect, useState} from 'react';
-const {Option} = Select;
+import React, { useEffect, useState } from 'react';
+import {
+    Form,
+    Input,
+    Button,
+    Select
+} from 'antd';
 
 const PlayFilter = (props) => {
 
@@ -17,7 +19,7 @@ const PlayFilter = (props) => {
      */
     const processFilter = () => {
         if ((filterBeforeYear !== "" && isNaN(filterBeforeYear)) ||
-                (filterAfterYear !=="" && isNaN(filterAfterYear))) {
+            (filterAfterYear !== "" && isNaN(filterAfterYear))) {
             alert("Please put a valid year for the year filters.");
             return;
         }
@@ -48,21 +50,57 @@ const PlayFilter = (props) => {
 
     return (
         <div className="filter">
-            <h2>Filters</h2>
+            <Form
+                labelCol={{
+                    span: 4,
+                }}
+                wrapperCol={{
+                    span: 14,
+                }}
+                layout="horizontal"
+            >
+                <Form.Item label="Title">
+                    <Input name="title-filter" onChange={handleChange} class="filter-input" />
+                </Form.Item>
+                <Form.Item label="Before">
+                    <Input name="before-year-filter" onChange={handleChange} className="filter-input" />
+                </Form.Item>
+                <Form.Item label="After">
+                    <Input name="after-year-filter" onChange={handleChange} className="filter-input" />
+                </Form.Item>
+                <Form.Item label="Genre">
+                    <Select name="genre-filter" defaultValue="" className="filter-input" onChange={(value) => {
+                        updateFilterGenre(value);
+                    }}>
+                        <Select.Option value=""></Select.Option>
+                        <Select.Option value="comedy">Comedy</Select.Option>
+                        <Select.Option value="tragedy">Tragedy</Select.Option>
+                        <Select.Option value="history">History</Select.Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" class="filter-button" onClick={processFilter}>Filter</Button><Button class="clear-filter-button" onClick={clearFilter}>Clear</Button>
+                </Form.Item>
+            </Form>
+
+
+
+
+            {/* <h2>Filters</h2>
             <br/>
             <label htmlFor="title-filter" className="filter-label">Title:</label>
             {/* <input type="text" name="title-filter" className="filter-input" onChange={handleChange}></input> */}
-            <Input name="title-filter" size="small" onChange={handleChange}/>
+            {/* <Input name="title-filter" size="small" onChange={handleChange}/> */}
 
-            <label htmlFor="before-year-filter" className="filter-label">Before year:</label>
+            {/* <label htmlFor="before-year-filter" className="filter-label">Before year:</label> */}
             {/* <input type="text" name="before-year-filter" className="filter-input" onChange={handleChange}></input> */}
-            <Input name="before-year-filter" size="small" onChange={handleChange}/>
+            {/* <Input name="before-year-filter" size="small" onChange={handleChange}/> */}
 
-            <label htmlFor="after-year-filter" className="filter-label">After year:</label>
+            {/* <label htmlFor="after-year-filter" className="filter-label">After year:</label> */}
             {/* <input type="text" name="after-year-filter" className="filter-input" onChange={handleChange}></input> */}
-            <Input name="after-year-filter" size="small" onChange={handleChange}/>
+            {/* <Input name="after-year-filter" size="small" onChange={handleChange}/> */}
 
-            <label htmlFor="genre-filter" className="filter-label">Genre:</label>
+            {/* <label htmlFor="genre-filter" className="filter-label">Genre:</label> */}
             {/* <select name="genre-filter" defaultValue="none" className="filter-input" onChange={handleChange}>
                 <option value=""></option>
                 <option value="comedy">Comedy</option>
@@ -72,21 +110,21 @@ const PlayFilter = (props) => {
             {/**The only way to access the Select value with ant-design is to pass it into the onChange event,
              * which is why it has a different onChange implementation than the other inputs.
              */}
-            <Select name="genre-filter" defaultValue="" onChange={(value) => {
-                updateFilterGenre(value);
-            }}>
-                <Option value=""></Option>
+            {/* <Select name="genre-filter" defaultValue="" onChange={(value) => { */}
+            {/* updateFilterGenre(value); */}
+            {/* }}> */}
+            {/* <Option value=""></Option>
                 <Option value="comedy">Comedy</Option>
-                <Option value="tragedy">Tragedy</Option>
-                <Option value="history">History</Option>
-            </Select>
+                <Option value="tragedy">Tragedy</Option> */}
+            {/* <Option value="history">History</Option>
+            </Select> */}
 
             {/* <button className="filter-button" onClick={processFilter}>Filter</button>
             <button className="clear-filter-button" onClick={clearFilter}>Clear</button> */}
-            <Space align="end">
+            {/* <Space align="end">
                 <Button type="primary" size="large" className="filter-button" onClick={processFilter}>Filter</Button>
                 <Button type="primary" size="large" className="clear-filter-button" onClick={clearFilter}>Clear</Button>
-            </Space>
+            </Space> */}
         </div>
     )
 }
