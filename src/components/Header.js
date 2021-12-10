748fe161f8a58931dd4eba4cd3c03027a5532a35
Matthew Fudge <mfudg395@mtroyal.ca>
2021-12-09 21:20:11 -0700

    Added profile button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/images/logo.png";
 import { Link } from 'react-router-dom'; 
-import { Button } from "antd";
+import { Button, Drawer } from "antd";
 
 const Header = (props) => {
 
+    const [visible, setVisible] = useState(false);
+
     const handleClick = () => {
         props.aboutOnClick();
+    }
+
+    function showDrawer() {
+        setVisible(true);
+    }
+
+    function closeDrawer() {
+        setVisible(false);
     }
 
     return (
@@ -16,8 +26,12 @@ const Header = (props) => {
                 <Link to="/home">
                     <img src={Logo} title="Book Logo" alt="Book Logo" className="site-logo" />
                 </Link>
+                <Button className="profile-button" type="default" size="large" onClick={showDrawer}>Profile</Button>
                 <Button className="about-button" type="default" size="large" onClick={handleClick}>About</Button>
                 {/* <button className="about-button" onClick={handleClick}>About</button> */}
+                <Drawer title="Profile" placement="top" closable={true} visible={visible} onClose={closeDrawer}>
+
+                </Drawer>
             </div>
         </header>
     );
